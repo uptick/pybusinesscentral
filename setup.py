0221f5fcbb29530f89bc79e6fc8cee2a80e922cd
Aden McCusker <aden.m@null.net>
2025-07-17 11:15:21 +1000

    bump to v3.0.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages  # noqa: H301
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "pybusinesscentral"
-VERSION = "3.0.0"
+VERSION = "3.0.1"
 PYTHON_REQUIRES = ">= 3.9"
 REQUIRES = [
     "urllib3 >= 2.1.0, < 3.0.0",
