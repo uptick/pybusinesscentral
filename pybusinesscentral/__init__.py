5824f6c31abffa186821748f1c8e5343894268a1
Aden McCusker <aden.m@null.net>
2025-07-21 16:00:51 +1000

    fix: imports

--- pybusinesscentral/__init__.py
+++ pybusinesscentral/__init__.py
@@ -16,20 +16,6 @@
 
 __version__ = "1.0.0"
 
-# import apis into sdk package
-from pybusinesscentral.api.account_api import AccountApi
-from pybusinesscentral.api.company_api import CompanyApi
-from pybusinesscentral.api.customer_api import CustomerApi
-from pybusinesscentral.api.dimension_api import DimensionApi
-from pybusinesscentral.api.dimension_set_line_api import DimensionSetLineApi
-from pybusinesscentral.api.payment_term_api import PaymentTermApi
-from pybusinesscentral.api.purchase_invoice_api import PurchaseInvoiceApi
-from pybusinesscentral.api.sales_credit_memo_api import SalesCreditMemoApi
-from pybusinesscentral.api.sales_credit_memo_line_api import SalesCreditMemoLineApi
-from pybusinesscentral.api.sales_invoice_api import SalesInvoiceApi
-from pybusinesscentral.api.sales_invoice_line_api import SalesInvoiceLineApi
-from pybusinesscentral.api.vendor_api import VendorApi
-
 # import ApiClient
 from pybusinesscentral.api_response import ApiResponse
 from pybusinesscentral.api_client import ApiClient
@@ -41,100 +27,8 @@ from pybusinesscentral.exceptions import ApiKeyError
 from pybusinesscentral.exceptions import ApiAttributeError
 from pybusinesscentral.exceptions import ApiException
 
-# import models into sdk package
-from pybusinesscentral.model.account import Account
-from pybusinesscentral.model.aged_accounts_payable import AgedAccountsPayable
-from pybusinesscentral.model.aged_accounts_receivable import AgedAccountsReceivable
-from pybusinesscentral.model.attachments import Attachments
-from pybusinesscentral.model.balance_sheet import BalanceSheet
-from pybusinesscentral.model.bank_account import BankAccount
-from pybusinesscentral.model.cash_flow_statement import CashFlowStatement
-from pybusinesscentral.model.company import Company
-from pybusinesscentral.model.company_information import CompanyInformation
-from pybusinesscentral.model.country_region import CountryRegion
-from pybusinesscentral.model.currency import Currency
-from pybusinesscentral.model.customer import Customer
-from pybusinesscentral.model.customer_financial_detail import CustomerFinancialDetail
-from pybusinesscentral.model.customer_payment import CustomerPayment
-from pybusinesscentral.model.customer_payment_journal import CustomerPaymentJournal
-from pybusinesscentral.model.customer_sale import CustomerSale
-from pybusinesscentral.model.default_dimensions import DefaultDimensions
-from pybusinesscentral.model.dimension import Dimension
-from pybusinesscentral.model.dimension_line import DimensionLine
-from pybusinesscentral.model.dimension_set_line import DimensionSetLine
-from pybusinesscentral.model.dimension_value import DimensionValue
-from pybusinesscentral.model.dimensiontype import Dimensiontype
-from pybusinesscentral.model.documentlineobjectdetailstype import Documentlineobjectdetailstype
-from pybusinesscentral.model.employee import Employee
-from pybusinesscentral.model.general_ledger_entry import GeneralLedgerEntry
-from pybusinesscentral.model.general_ledger_entry_attachments import GeneralLedgerEntryAttachments
-from pybusinesscentral.model.general_ledger_entry_dimensions_inner import GeneralLedgerEntryDimensionsInner
-from pybusinesscentral.model.income_statement import IncomeStatement
-from pybusinesscentral.model.item import Item
-from pybusinesscentral.model.item_category import ItemCategory
-from pybusinesscentral.model.itemunitofmeasureconversiontype import Itemunitofmeasureconversiontype
-from pybusinesscentral.model.journal import Journal
-from pybusinesscentral.model.journal_line import JournalLine
-from pybusinesscentral.model.list_accounts200_response import ListAccounts200Response
-from pybusinesscentral.model.list_companies200_response import ListCompanies200Response
-from pybusinesscentral.model.list_customers200_response import ListCustomers200Response
-from pybusinesscentral.model.list_dimensions200_response import ListDimensions200Response
-from pybusinesscentral.model.list_payment_terms200_response import ListPaymentTerms200Response
-from pybusinesscentral.model.list_purchase_invoices200_response import ListPurchaseInvoices200Response
-from pybusinesscentral.model.list_sales_credit_memo_lines_for_sales_credit_memo200_response import ListSalesCreditMemoLinesForSalesCreditMemo200Response
-from pybusinesscentral.model.list_sales_credit_memos200_response import ListSalesCreditMemos200Response
-from pybusinesscentral.model.list_sales_invoice_lines_for_sales_invoice200_response import ListSalesInvoiceLinesForSalesInvoice200Response
-from pybusinesscentral.model.list_sales_invoices200_response import ListSalesInvoices200Response
-from pybusinesscentral.model.list_vendors200_response import ListVendors200Response
-from pybusinesscentral.model.payment_method import PaymentMethod
-from pybusinesscentral.model.payment_term import PaymentTerm
-from pybusinesscentral.model.pdf_document import PdfDocument
-from pybusinesscentral.model.picture import Picture
-from pybusinesscentral.model.post_customer_request import PostCustomerRequest
-from pybusinesscentral.model.post_dimension_set_line_request import PostDimensionSetLineRequest
-from pybusinesscentral.model.post_payment_term_request import PostPaymentTermRequest
-from pybusinesscentral.model.post_purchase_invoice_request import PostPurchaseInvoiceRequest
-from pybusinesscentral.model.post_sales_credit_memo_line_for_sales_credit_memo_request import PostSalesCreditMemoLineForSalesCreditMemoRequest
-from pybusinesscentral.model.post_sales_credit_memo_request import PostSalesCreditMemoRequest
-from pybusinesscentral.model.post_sales_invoice_line_for_sales_invoice_request import PostSalesInvoiceLineForSalesInvoiceRequest
-from pybusinesscentral.model.post_sales_invoice_request import PostSalesInvoiceRequest
-from pybusinesscentral.model.post_vendor_request import PostVendorRequest
-from pybusinesscentral.model.postaladdresstype import Postaladdresstype
-from pybusinesscentral.model.project import Project
-from pybusinesscentral.model.purchase_invoice import PurchaseInvoice
-from pybusinesscentral.model.purchase_invoice_line import PurchaseInvoiceLine
-from pybusinesscentral.model.retained_earnings_statement import RetainedEarningsStatement
-from pybusinesscentral.model.sales_credit_memo import SalesCreditMemo
-from pybusinesscentral.model.sales_credit_memo_line import SalesCreditMemoLine
-from pybusinesscentral.model.sales_invoice import SalesInvoice
-from pybusinesscentral.model.sales_invoice_line import SalesInvoiceLine
-from pybusinesscentral.model.sales_order import SalesOrder
-from pybusinesscentral.model.sales_order_line import SalesOrderLine
-from pybusinesscentral.model.sales_quote import SalesQuote
-from pybusinesscentral.model.sales_quote_line import SalesQuoteLine
-from pybusinesscentral.model.shipment_method import ShipmentMethod
-from pybusinesscentral.model.tax_area import TaxArea
-from pybusinesscentral.model.tax_group import TaxGroup
-from pybusinesscentral.model.time_registration_entry import TimeRegistrationEntry
-from pybusinesscentral.model.trial_balance import TrialBalance
-from pybusinesscentral.model.unit_of_measure import UnitOfMeasure
-from pybusinesscentral.model.unitofmeasuretype import Unitofmeasuretype
-from pybusinesscentral.model.vendor import Vendor
-from pybusinesscentral.model.vendor_purchase import VendorPurchase
 
 __all__ = [
-    "AccountApi",
-    "CompanyApi",
-    "CustomerApi",
-    "DimensionApi",
-    "DimensionSetLineApi",
-    "PaymentTermApi",
-    "PurchaseInvoiceApi",
-    "SalesCreditMemoApi",
-    "SalesCreditMemoLineApi",
-    "SalesInvoiceApi",
-    "SalesInvoiceLineApi",
-    "VendorApi",
     "ApiResponse",
     "ApiClient",
     "Configuration",
@@ -144,83 +38,4 @@ __all__ = [
     "ApiKeyError",
     "ApiAttributeError",
     "ApiException",
-    "Account",
-    "AgedAccountsPayable",
-    "AgedAccountsReceivable",
-    "Attachments",
-    "BalanceSheet",
-    "BankAccount",
-    "CashFlowStatement",
-    "Company",
-    "CompanyInformation",
-    "CountryRegion",
-    "Currency",
-    "Customer",
-    "CustomerFinancialDetail",
-    "CustomerPayment",
-    "CustomerPaymentJournal",
-    "CustomerSale",
-    "DefaultDimensions",
-    "Dimension",
-    "DimensionLine",
-    "DimensionSetLine",
-    "DimensionValue",
-    "Dimensiontype",
-    "Documentlineobjectdetailstype",
-    "Employee",
-    "GeneralLedgerEntry",
-    "GeneralLedgerEntryAttachments",
-    "GeneralLedgerEntryDimensionsInner",
-    "IncomeStatement",
-    "Item",
-    "ItemCategory",
-    "Itemunitofmeasureconversiontype",
-    "Journal",
-    "JournalLine",
-    "ListAccounts200Response",
-    "ListCompanies200Response",
-    "ListCustomers200Response",
-    "ListDimensions200Response",
-    "ListPaymentTerms200Response",
-    "ListPurchaseInvoices200Response",
-    "ListSalesCreditMemoLinesForSalesCreditMemo200Response",
-    "ListSalesCreditMemos200Response",
-    "ListSalesInvoiceLinesForSalesInvoice200Response",
-    "ListSalesInvoices200Response",
-    "ListVendors200Response",
-    "PaymentMethod",
-    "PaymentTerm",
-    "PdfDocument",
-    "Picture",
-    "PostCustomerRequest",
-    "PostDimensionSetLineRequest",
-    "PostPaymentTermRequest",
-    "PostPurchaseInvoiceRequest",
-    "PostSalesCreditMemoLineForSalesCreditMemoRequest",
-    "PostSalesCreditMemoRequest",
-    "PostSalesInvoiceLineForSalesInvoiceRequest",
-    "PostSalesInvoiceRequest",
-    "PostVendorRequest",
-    "Postaladdresstype",
-    "Project",
-    "PurchaseInvoice",
-    "PurchaseInvoiceLine",
-    "RetainedEarningsStatement",
-    "SalesCreditMemo",
-    "SalesCreditMemoLine",
-    "SalesInvoice",
-    "SalesInvoiceLine",
-    "SalesOrder",
-    "SalesOrderLine",
-    "SalesQuote",
-    "SalesQuoteLine",
-    "ShipmentMethod",
-    "TaxArea",
-    "TaxGroup",
-    "TimeRegistrationEntry",
-    "TrialBalance",
-    "UnitOfMeasure",
-    "Unitofmeasuretype",
-    "Vendor",
-    "VendorPurchase",
 ]
